55358e2f1f66b3d88965cc8f4802f824e330fa71
David de Esteban <estdav@protonmail.com>
2025-09-04 14:11:58 +0200

    Implements hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { LaptopMinimalCheck } from "lucide-react";
-import ThemeChanger from "./theme-changer";
+import Hero from "@/components/custom/sections/hero";
 
 export default function Home ({
   children,
@@ -8,12 +6,9 @@ export default function Home ({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="font-sans bg-background h-[3000px]">
-      <main className="flex flex-col items-center sm:items-start">
-        <Button>Test<LaptopMinimalCheck /></Button>
-        <Button variant="secondary">Test<LaptopMinimalCheck /></Button>
-        <Button variant="link">Test<LaptopMinimalCheck /></Button>
-        <ThemeChanger />
+    <div className="font-sans bg-background">
+      <main className="flex flex-col items-center">
+        <Hero />
         {children}
       </main>
     </div>
